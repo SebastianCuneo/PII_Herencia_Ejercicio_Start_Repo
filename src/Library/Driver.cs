bb6735f73a3da1969403d64609c1bac5d541f05c
SebastianCuneo <sebastian.cuneo.s1@gmail.com>
2024-10-15 12:02:37 -0300

    creacion de Driver

---
+++ src/Library/Driver.cs
@@ -0,0 +1,17 @@
+﻿namespace Library;
+
+public class Driver : User
+{
+    public string Vehicle { get; set; }
+    public int Qualification { get; set; }
+    
+    
+    
+    // Constructor
+    public Driver(string nombre, string cedula, string apellido, string vehicle, int qualification): base(nombre, cedula, apellido)
+    {
+        Vehicle = vehicle;
+        Qualification = qualification;
+    }
+}
+
