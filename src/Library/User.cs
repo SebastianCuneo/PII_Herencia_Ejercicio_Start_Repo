58fb0cb118319524650b2e5210a7872775604569
SebastianCuneo <sebastian.cuneo.s1@gmail.com>
2024-10-15 11:43:41 -0300

    Creacion de user

---
+++ src/Library/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Library;
+
+public class User
+{
+    // Propiedades de la clase
+    public string Nombre { get; set; }
+    public string CorreoElectronico { get; set; }
+    public List<string> Roles { get; private set; }
+
+    // Constructor
+    public User(string nombre, string correoElectronico)
+    {
+        Nombre = nombre;
+        CorreoElectronico = correoElectronico;
+        Roles = new List<string>();
+    }
+}
